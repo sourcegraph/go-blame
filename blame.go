4e3ad5428009499446858d400d5b82fbf58a2a9a
Beyang Liu <beyang.liu@gmail.com>
2013-10-07 18:49:25 -0700

    file blame

--- blame.go
+++ blame.go
@@ -2,15 +2,11 @@ package blame
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 )
-
-// type File struct {
-// 	Path  string
-// 	Hunks []Hunk
-// }
 
 type Hunk struct {
 	CommitID  string
@@ -30,19 +26,25 @@ type Author struct {
 	Email string
 }
 
-func BlameFile(filePath string) ([]*Hunk, map[string]*Commit, error) {
-	out, err := exec.Command("git", "blame", "--porcelain", "--", filePath).Output()
+// Note: filePath should be absolute or relative to repoPath
+func BlameFile(repoPath string, filePath string) ([]Hunk, map[string]Commit, error) {
+	cmd := exec.Command("git", "blame", "--porcelain", "--", filePath)
+	cmd.Dir = repoPath
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	commits := make(map[string]*Commit)
-	hunks := make([]*Hunk, 0)
-	remainingLines := strings.Split(string(out), "\n")
+	commits := make(map[string]Commit)
+	hunks := make([]Hunk, 0)
+	remainingLines := strings.Split(strings.TrimSpace(string(out)), "\n")
+	charOffset := 0
 	for len(remainingLines) > 0 {
 		// Consume hunk
 		hunkHeader := strings.Split(remainingLines[0], " ")
 		if len(hunkHeader) != 4 {
+			fmt.Printf("Remaining lines: %+v, %d, '%s'\n", remainingLines, len(remainingLines), remainingLines[0])
 			return nil, nil, fmt.Errorf("Expected at least 4 parts to hunkHeader, but got: '%s'", hunkHeader)
 		}
 		commitID := hunkHeader[0]
@@ -50,38 +52,46 @@ func BlameFile(filePath string) ([]*Hunk, map[string]*Commit, error) {
 		nLines, _ := strconv.Atoi(hunkHeader[3])
 		hunk := Hunk{
 			CommitID:  commitID,
-			LineStart: int(lineNoCur),
-			LineEnd:   int(lineNoCur + nLines),
-			// TODO: LineEnd
-			// TODO: CharStart/End
+			LineStart: int(lineNoCur) - 1,
+			LineEnd:   int(lineNoCur + nLines - 1),
+			CharStart: charOffset,
 		}
 
 		if _, in := commits[commitID]; in {
 			// Already seen commit
+			charOffset += len(remainingLines[1])
 			remainingLines = remainingLines[2:]
 		} else {
 			// New commit
 			author := strings.Join(strings.Split(remainingLines[1], " ")[1:], " ")
-			commits[commitID] = &Commit{
+			email := strings.Join(strings.Split(remainingLines[2], " ")[1:], " ")
+			commits[commitID] = Commit{
 				ID: commitID,
 				Author: Author{
-					Name: author,
+					Name:  author,
+					Email: email,
 				},
 			}
-
-			if strings.HasPrefix(remainingLines[10], "previous") {
+			if strings.HasPrefix(remainingLines[10], "previous ") {
+				charOffset += len(remainingLines[12])
+				remainingLines = remainingLines[13:]
+			} else if remainingLines[10] == "boundary" {
+				charOffset += len(remainingLines[12])
 				remainingLines = remainingLines[13:]
 			} else {
+				charOffset += len(remainingLines[11])
 				remainingLines = remainingLines[12:]
 			}
 		}
 
 		// Consume remaining lines in hunk
 		for i := 1; i < nLines; i++ {
+			charOffset += len(remainingLines[1])
 			remainingLines = remainingLines[2:]
 		}
 
-		hunks = append(hunks, &hunk)
+		hunk.CharEnd = charOffset
+		hunks = append(hunks, hunk)
 	}
 
 	return hunks, commits, nil
--- blame_test.go
+++ blame_test.go
@@ -1,9 +1,51 @@
 package blame
 
 import (
+	// "os"
+	// "path/filepath"
+	"reflect"
 	"testing"
 )
 
-func TestBlame(t *testing.T) {
+var testRepoDir = "goblametest"
 
+func TestBlameFile(t *testing.T) {
+	hunks, commits, err := BlameFile(testRepoDir, "goblametest.go")
+	if err != nil {
+		t.Fatalf("Failed to compute blame: %v", err)
+	}
+
+	expHunks := []Hunk{
+		{CommitID: "26e6e00a6bfd5430a5a8840a543465dc8cac801e", LineStart: 0, LineEnd: 1, CharStart: 0, CharEnd: 20},
+		{CommitID: "c497236203ba6400272034a9db7be00859c9863d", LineStart: 1, LineEnd: 2, CharStart: 20, CharEnd: 21},
+		{CommitID: "7653ddfbc69a584272a18fe5e675b95025e84bb9", LineStart: 2, LineEnd: 4, CharStart: 21, CharEnd: 37},
+		{CommitID: "d858245d0690b83df437ad830ab1e971d389d68d", LineStart: 4, LineEnd: 5, CharStart: 37, CharEnd: 43},
+		{CommitID: "7653ddfbc69a584272a18fe5e675b95025e84bb9", LineStart: 5, LineEnd: 6, CharStart: 43, CharEnd: 45},
+	}
+	expCommits := map[string]Commit{
+		"26e6e00a6bfd5430a5a8840a543465dc8cac801e": {
+			ID:     "26e6e00a6bfd5430a5a8840a543465dc8cac801e",
+			Author: Author{Name: "Beyang Liu", Email: "<beyang.liu@gmail.com>"},
+		},
+		"c497236203ba6400272034a9db7be00859c9863d": {
+			ID:     "c497236203ba6400272034a9db7be00859c9863d",
+			Author: Author{Name: "Beyang Liu", Email: "<beyang.liu@gmail.com>"},
+		},
+		"7653ddfbc69a584272a18fe5e675b95025e84bb9": {
+			ID:     "7653ddfbc69a584272a18fe5e675b95025e84bb9",
+			Author: Author{Name: "Ricky Bobby", Email: "<ricky@bobby.com>"},
+		},
+		"d858245d0690b83df437ad830ab1e971d389d68d": {
+			ID:     "d858245d0690b83df437ad830ab1e971d389d68d",
+			Author: Author{Name: "Sam Hamilton", Email: "<sam@salinas.com>"},
+		},
+	}
+
+	if !reflect.DeepEqual(expHunks, hunks) {
+		t.Errorf("Expected hunks: %+v, but got %+v", expHunks, hunks)
+	}
+
+	if !reflect.DeepEqual(expCommits, commits) {
+		t.Errorf("Expected commits: %+v, but got %+v", expCommits, commits)
+	}
 }
